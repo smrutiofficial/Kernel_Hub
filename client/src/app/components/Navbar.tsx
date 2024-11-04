181ae4938b5fc0d88e973c61e7d285c9c72be85d
smrutiofficial <smrutiprakashrout3@gmail.com>
2024-11-05 00:15:04 +0530

    jhghlll

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import Image from "next/image";
 import ProfilePic from "../image/profile-pic.png";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname(); // Get the current pathname
 
   const handleLogout = () => {
     // Remove token from localStorage
@@ -19,54 +20,83 @@ const Navbar = () => {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
-        <p className="text-white text-3xl font-bold">
-          Kernel <span className="text-[#AAFFA9]">Hub</span>
-        </p>
+          <p className="text-white text-3xl font-bold">
+            Kernel <span className="text-[#AAFFA9]">Hub</span>
+          </p>
         </Link>
         <ul className="flex space-x-4">
           <li>
             <Link href="/">
-            <p className="text-transparent bg-clip-text bg-gradient-to-r from-[#AAFFA9] to-emerald-500">
-              Home
-            </p>
+              <p
+                className={`${
+                  pathname === "/" 
+                    ? "text-transparent bg-clip-text bg-gradient-to-r from-[#AAFFA9] to-emerald-500"
+                    : "text-gray-400 hover:text-yellow-300"
+                }`}
+              >
+                Home
+              </p>
             </Link>
           </li>
 
           <li>
             <Link href="/resources">
-            <p className="text-gray-400 hover:text-yellow-300">
-              Resources
-            </p>
+              <p
+                className={`${
+                  pathname === "/resources"
+                    ? "text-transparent bg-clip-text bg-gradient-to-r from-[#AAFFA9] to-emerald-500"
+                    : "text-gray-400 hover:text-yellow-300"
+                }`}
+              >
+                Resources
+              </p>
             </Link>
           </li>
           <li>
             <Link href="/community">
-            <p className="text-gray-400 hover:text-yellow-300">
-              Community
-            </p>
+              <p
+                className={`${
+                  pathname === "/community"
+                    ? "text-transparent bg-clip-text bg-gradient-to-r from-[#AAFFA9] to-emerald-500"
+                    : "text-gray-400 hover:text-yellow-300"
+                }`}
+              >
+                Community
+              </p>
             </Link>
           </li>
           <li>
             <Link href="/feedback">
-            <p className="text-gray-400 hover:text-yellow-300">
-              Feedback
-            </p>
+              <p
+                className={`${
+                  pathname === "/feedback"
+                    ? "text-transparent bg-clip-text bg-gradient-to-r from-[#AAFFA9] to-emerald-500"
+                    : "text-gray-400 hover:text-yellow-300"
+                }`}
+              >
+                Feedback
+              </p>
             </Link>
           </li>
           <li>
             <Link href="/about">
-            <p className="text-gray-400 hover:text-yellow-300">
-              About
-            </p>
+              <p
+                className={`${
+                  pathname === "/about"
+                    ? "text-transparent bg-clip-text bg-gradient-to-r from-[#AAFFA9] to-emerald-500"
+                    : "text-gray-400 hover:text-yellow-300"
+                }`}
+              >
+                About
+              </p>
             </Link>
           </li>
         </ul>
         <div className="flex flex-row gap-4 justify-center items-center">
-          <button
-            onClick={handleLogout}
-            className="button-64"
-          >
-             <span className="text-sm py-2 px-6 font-medium hover:text-gray-700 hover:font-medium">Logout</span>
+          <button onClick={handleLogout} className="button-64">
+            <span className="text-sm py-2 px-6 font-medium hover:text-gray-700 hover:font-medium">
+              Logout
+            </span>
           </button>
           <div className="h-9 w-9 overflow-hidden flex justify-center items-center ">
             <Image
--- client/src/app/feedback/page.tsx
+++ client/src/app/feedback/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/footer';
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <div className="w-svw h-8 bg-gradient-to-r from-[#AAFFA9] to-emerald-500 flex justify-center items-center relative">
+      <p className="text-gray-600 font-medium">
+        Ubuntu{" "}
+        <span className="text-yellow-300 [text-shadow:_0_2px_4px_rgb(0_0_0_/_0.5)]">
+          24.10{" "}
+        </span>
+        loaded with new features 👋🏻! let&apos;s view new updates .🥳
+      </p>
+    </div>
+    <Navbar/>
+    feedback page
+    <Footer/>
+    </>
   )
 }
 
